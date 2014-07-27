5e88e29f8686167a46ef9b1b8bd88540fc2e300d
Christian Busch <gizmore@giz.org>
2014-07-27 03:02:46 +0200

    Fixed autologin by Nickserv

--- app/models/ricer/plugins/auth/autologin.rb
+++ app/models/ricer/plugins/auth/autologin.rb
@@ -39,7 +39,7 @@ module Ricer::Plugins::Auth
     end
     
     def autologin(username)
-      user = Ricer::Irc::User.online.where(:nickname => username, server_id: server.id)
+      user = Ricer::Irc::User.online.where(:nickname => username, server_id: server.id).first
       unless user.nil?
         user.login!
         user.localize!.send_message(I18n.t('ricer.plugins.auth.autologin.msg_logged_in'))
