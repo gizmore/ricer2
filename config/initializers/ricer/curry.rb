2f426afe68b7b6658c770cb94ded4914c1fdda08
Christian Busch <gizmore@giz.org>
2014-09-11 02:11:54 +0200

    Curry cleanup and debug puts

--- config/initializers/ricer/curry.rb
+++ config/initializers/ricer/curry.rb
@@ -1,19 +1,21 @@
+#
+# Enable a global cache table for active record models.
+# Does slow everything down a bit!
+#
 module ActiveRecord
 
   class Base
     def self.with_global_orm_mapping
-      class_eval do |cl|
-        cl.class_variable_set('@@CACHE', {}) unless cl.class_variable_defined?('@@CACHE')
-        def global_cache_table(record)
-          c = self.class.class_variable_get('@@CACHE')
-          c[record.id] ||= record if record.should_cache?
-#          record = c[record.id] || record ## DEBUG
-#          puts "Curry#get_cached: '#{record.class.name}'#{record.id} with object_id #{record.object_id}." ## DEBUG
-#          record ## DEBUG
-          c[record.id] || record
-        end
-        def self.global_cache
+      class_eval do |klass|
+        klass.class_variable_set('@@CACHE', {}) unless klass.class_variable_defined?('@@CACHE')
+        def global_cache
           self.class.class_variable_get('@@CACHE')
+        end
+        def global_cache_table(record)
+          c = global_cache
+          c[record.id] ||= record if record.should_cache?
+#          record = c[record.id] || record; puts "Curry#get_cached: '#{record.class.name}'#{record.id} with object_id #{record.object_id}." ## DEBUG
+          c[record.id] || record
         end
         def global_cache_add
 #          puts "Curry#global_cache_add: '#{self.class.name}'#{self.id} with object_id #{self.object_id}."
@@ -21,7 +23,7 @@ module ActiveRecord
         end
         def global_cache_remove
 #          puts "Curry#global_cache_remove: '#{self.class.name}'#{self.id} with object_id #{self.object_id}."
-          self.class.global_cache.delete(self.id)
+          global_cache.delete(self.id)
         end
       end
     end
@@ -29,18 +31,19 @@ module ActiveRecord
 
   class Relation
     
-    alias :original_exec_queries :exec_queries
+    alias :original_exec_queries :exec_queries # alias to override
     
+    # SLOOOW
     def exec_queries
       begin
         new_records = []
-        original_exec_queries.each do |record|
+        original_exec_queries.each do |record| # call overridden
           break unless record.respond_to? :global_cache_table
           new_records.push(record.global_cache_table(record))
         end
         @records = new_records unless new_records.empty?
-      rescue => e
-        #puts "OOOPS: #{e.to_s}\n\n#{e.backtrace}\n"
+      rescue StandardError => e
+        puts "OOOPS: #{e.to_s}\n\n#{e.backtrace}\n"
       end
       @records
     end
