fcae6468f84d24f65e8a0cebf8c96f87d01de296
roun512 <roun.miler@gmail.com>
2014-09-13 03:37:09 +0300

    Update connect.rb

--- app/models/ricer/plugins/tcp/connect.rb
+++ app/models/ricer/plugins/tcp/connect.rb
@@ -7,16 +7,19 @@ module Ricer::Plugins::Tcp
   class Connect < Ricer::Plugin
 
     permission_is :voice
-    
+
     trigger_is :connect
 
     has_usage :connect, '<host> <port>'
 
     def connect(host, port)
       tcp_connection = sender.instance_variable_get(:@ricer_tcp_plugin_connection)
-      get_plugin('Tcp/Close').execute_close unless tcp_connection.nil?
-      begin
-        Ricer::Thread.execute do
+      unless tcp_connection.nil?
+        get_plugin('Tcp/Close').execute_close
+        reply "Previous connection closed"
+      end
+      Ricer::Thread.execute do
+        begin
           socket = TCPSocket.open(host, port)
           if socket
             reply "Connected"
@@ -24,13 +27,17 @@ module Ricer::Plugins::Tcp
             reply "Couldn't connect"
           end
 
+
+       # response = socket.read
+       # reply response
+
         #user.instance_variable_set(:@ricer_tcp_plugin_connection, socket)
           sender.instance_variable_set(:@ricer_tcp_plugin_connection, socket)
           sender.instance_variable_set(:@ricer_tcp_plugin_host, host)
           sender.instance_variable_set(:@ricer_tcp_plugin_port, port)
+        rescue => se
+          byebug
         end
-      rescue SocketError => se
-        reply se
       end
     end
   end
