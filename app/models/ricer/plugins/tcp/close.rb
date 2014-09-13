5e00af9ef8e359349d1ae375f893d044431f9b32
roun512 <roun.miler@gmail.com>
2014-09-13 03:38:55 +0300

    Update close.rb

--- app/models/ricer/plugins/tcp/close.rb
+++ app/models/ricer/plugins/tcp/close.rb
@@ -8,7 +8,10 @@ module Ricer::Plugins::Tcp
   class Close < Ricer::Plugin
     def execute_close
       tcp_connection = sender.instance_variable_get(:@ricer_tcp_plugin_connection)
-      tcp_connection.close unless tcp_connection.nil?
+      unless tcp_connection.nil?
+        tcp_connection.close
+        reply "Previous connection closed"
+      end
     end
   end
 end
