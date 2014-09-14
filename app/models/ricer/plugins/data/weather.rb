77071f343645dcf7ccbc54cd16a0cc7c36f9cb5a
Christian Busch <gizmore@giz.org>
2014-09-14 04:03:20 +0200

    Corrections to weather plugin

--- app/models/ricer/plugins/data/weather.rb
+++ app/models/ricer/plugins/data/weather.rb
@@ -11,13 +11,13 @@ module Ricer::Plugins::Data
       Ricer::Thread.execute do 
         url = "http://api.openweathermap.org/data/2.5/forecast?q=#{URI::encode(input)}&mode=xml"
         doc = Nokogiri::XML(open(url), nil, "UTF-8") rescue (raise t(:err_open_weather_connect))
-        data = doc.xpath("//temperature").first or (return rply :err_open_weather_input)
+        temperature = doc.xpath("//temperature").first or (return rply :err_open_weather_input)
         byebug
         rply :rpl_open_weather,
-          city: 'CITY',
-          temp_avg: data.attr("temp"),
-          temp_min: data.attr("min"),
-          temp_max: data.attr("max")
+          city: doc.xpath("//location/name").inner_html,
+          temp_avg: temperature.attr("value"),
+          temp_min: temperature.attr("min"),
+          temp_max: temperature.attr("max")
       end
     end
     
