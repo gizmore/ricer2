bdf2ffe02685a63303acc221a091c7faf5cf3912
Christian Busch <gizmore@giz.org>
2014-09-14 03:51:36 +0200

    Fixed translator task to produce 2 files per language, one huge, one for production release.

--- app/models/translator/translator.rb
+++ app/models/translator/translator.rb
@@ -1,3 +1,4 @@
+# Create huge lang files from the :en: snippets cluttered along the app
 module Translator
   class Translator
     
@@ -18,20 +19,20 @@ module Translator
       self.dir = default_dir unless defined?(@dir)
       target = target.to_sym
       return true if target == @source
-      byebug
       I18n.t(:trigger_loading_by_this_lol)
       translations = I18n.backend.instance_variable_get(:@translations)
       translations[target] ||= {}
+      save_generated(target, translations[target])
       sort_translations(translations[@source], translations[target])
       generate_recursive(translations[@source], translations[target])
-      save_generated(target, translations[target])
+      save_generated(target, translations[target], '_huge_')
     end
     
     def sort_translations(*hashes)
-      hashes.flatten.each{|hash|hash.sort_by_key(true)}
+      hashes.flatten.each{|hash|hash.sort_by_key(true) unless hash.nil?}
     end
     
-    def generate_recursive(source, dest) 
+    def generate_recursive(source, dest)
       source.each do |key, src|
         if src.is_a?(Hash)
           dest[key] ||= {}
@@ -42,12 +43,12 @@ module Translator
       end
     end
     
-    def save_generated(target, dest)
+    def save_generated(target, dest, prefix='')
       sort_translations(dest)
-      path = "#{@dir}/#{target}.yml"
-      File.open(path, 'w') {|f| f.write dest.to_yaml }
+      dest = nil if dest.empty?
+      path = "#{@dir}/#{prefix}#{target}.yml"
+      File.open(path, 'w') {|f| f.write({target => dest}.to_yaml) }
     end
-
     
   end
 end
