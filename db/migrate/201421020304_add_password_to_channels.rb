b8fad1f209f46ab52a0604afb9bd24bc04b4b188
Christian Busch <gizmore@giz.org>
2014-09-27 07:27:26 +0200

    Migrations were messed up

--- db/migrate/201421020304_add_password_to_channels.rb
+++
@@ -1,7 +0,0 @@
-class AddPasswordToChannels < ActiveRecord::Migration
-  def change
-    
-    add_column :channels, :password, :string, :after => :name, :limit => 64, :null => true, :default => nil
-    
-  end
-end
