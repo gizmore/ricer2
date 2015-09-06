6c136d0e38127f8a740459b798c447dd6f1341f3
gizmore <gizmore@wechall.net>
2015-09-06 06:28:20 +0200

    repo abbos via name

--- app/models/ricer/plugins/cvs/abbo.rb
+++ app/models/ricer/plugins/cvs/abbo.rb
@@ -2,6 +2,10 @@ module Ricer::Plugins::Cvs
   class Abbo < Ricer::Plugin
     
     is_add_abbo_trigger :for => Ricer::Plugins::Cvs::Repo
-    
+
+    def abbo_find(relation, term)
+      relation.where(:name => term).first or relation.find(term)
+    end
+
   end
 end
--- app/models/ricer/plugins/cvs/unabbo.rb
+++ app/models/ricer/plugins/cvs/unabbo.rb
@@ -3,5 +3,9 @@ module Ricer::Plugins::Cvs
     
     is_remove_abbo_trigger :for => Ricer::Plugins::Cvs::Repo
 
+    def abbo_find(relation, term)
+      relation.where(:name => term).first or relation.find(term)
+    end
+
   end
 end
